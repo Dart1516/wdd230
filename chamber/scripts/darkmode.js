2361300ebdbfe621709d47a20d70170f5cf5d121
Dart1516 <Danvddpf1@gmail.com>
2024-02-15 17:43:53 -0300

    todo funciona en el local

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -11,11 +11,11 @@ ButtonImage.addEventListener("click", () => {
   ButtonImage.style.opacity = 0; // Reducir la opacidad
   setTimeout(() => {
     if (imagenActual === 1) {
-      ButtonImage.src = "img/img2.png";
+      ButtonImage.src = "/img/img2.png";
       imagenActual = 2;
       
     } else {
-      ButtonImage.src = "img/img1.png";
+      ButtonImage.src = "/img/img1.png";
       imagenActual = 1;
     }
     ButtonImage.style.opacity = 1; // Restaurar la opacidad después del cambio
