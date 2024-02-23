b6709a1297295702ba1f13d67cf75680e98e0a20
Dart1516 <Danvddpf1@gmail.com>
2024-02-23 19:53:47 -0300

    test

--- scripts/pageVisits.js
+++ scripts/pageVisits.js
@@ -1,22 +1,15 @@
-// initialize display elements
-const todayDisplay = document.querySelector(".today");
-const visitDisplay = document.querySelector(".visits");
+/*is inside of a fuction to be sure of load without error*/
+document.addEventListener("DOMContentLoaded", function () {
+  const visitDisplay = document.querySelector(".visits");
 
-//get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
+  let numVisits = Number(window.localStorage.getItem("visits-ls")) || 0;
 
-//determine if this is the first visit or display the number of visits
+  if (numVisits !== 0) {
+    visitDisplay.textContent = numVisits;
+  } else {
+    visitDisplay.textContent = "This is your first visit!";
+  }
 
-if (numVisits !==0) {
-	visitDisplay.textContent = numVisits;	
-} else {
-	visitDisplay.textContent='This is your first visit!';
-}
-
-// increment of visit
-numVisits++
-
-//store the new number of visit value
-localStorage.setItem("visits-ls", numVisits)
-
-todayDisplay.textContent=Date.now();
+  numVisits++;
+  localStorage.setItem("visits-ls", numVisits);
+});
