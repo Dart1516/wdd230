16800d46efa4f950f810101726ec360e3bb77dad
Dart1516 <Danvddpf1@gmail.com>
2024-03-04 02:00:17 -0300

    no audit error 2

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -4,7 +4,3 @@ year.textContent = new Date().getFullYear();
 const lastModifiedDate = document.getElementById('lastModified');
 lastModifiedDate.textContent = document.lastModified;
 
-const dateTimeHidden = document.getElementById('date-time');
-const currentDateTime = Date.now();
-dateTimeHidden.value = currentDateTime;
-console.log(dateTimeHidden.value)
