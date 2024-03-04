f00706af93b76a74f6e63da41f51bc8a0eed0857
Dart1516 <Danvddpf1@gmail.com>
2024-03-04 19:26:16 -0300

    password validation add it

--- scripts/form.js
+++ scripts/form.js
@@ -8,3 +8,23 @@ range.addEventListener('input', displayRatingValue);
 function displayRatingValue() {
     rangevalue.innerHTML = range.value;
 }
+
+// This is the code for Password validation
+const pwd1 = document.querySelector("#pwd");
+const pwd2 = document.querySelector("#pwd2");
+const msg = document.querySelector("#formmessage2");
+
+pwd2.addEventListener("input", checkSame2);
+
+function checkSame2() {
+    if (pwd1.value !== pwd2.value) {
+        msg.textContent = " Passwords do not match❗";
+        msg.style.visibility = "visible"; // Cambiado de "show" a "visible"
+        pwd2.style.backgroundColor = "#fff0f3";
+    } else {
+        msg.textContent = ""; // Limpiar el mensaje cuando las contraseñas coinciden
+        msg.style.visibility = "hidden"; // Ocultar el mensaje cuando las contraseñas coinciden
+        pwd2.style.backgroundColor = "#fff";
+        pwd2.style.color = "#000";
+    }
+}
