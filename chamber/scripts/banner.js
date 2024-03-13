6865d2c677c46eafb42dc624227aa16fcc9c6952
Dart1516 <Danvddpf1@gmail.com>
2024-03-13 16:34:43 -0300

    closed banner on monday till wednesday

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,4 +1,18 @@
-function closeBanner() { // Function to close the banner
-  document.getElementById("popupContainer").style.display = "none";
-
+function showBanner() {
+  document.getElementById("popupContainer").style.display = "block";
 }
+function closeBanner() { 
+  document.getElementById("popupContainer").style.display = "none";
+}
+
+function showBannerOnSpecificDays() {
+  var currentDate = new Date();
+  var currentDay = currentDate.getDay(); // Get the current day of the week (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
+  if (currentDay >= 1  && currentDay <= 3) {
+    
+  } else {
+    showBanner(); 
+    closeBanner(); 
+  }
+}
+showBannerOnSpecificDays();
