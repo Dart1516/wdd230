8ff5489a97925457efbd5761c7358b0e2d12519e
Dart1516 <Danvddpf1@gmail.com>
2024-03-22 03:41:18 -0300

    dinamic spotlight

---
+++ chamber/scripts/spotlight.js
@@ -0,0 +1,44 @@
+const urlJson = 'https://dart1516.github.io/wdd230/chamber/data/members.json';
+
+async function getData(url) {
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching JSON:', error);
+    }
+}
+
+// to select a random gold member
+function selectRandom(array, count) {
+    const shuffled = array.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, count);
+}
+
+// a random member each time
+function updateMemberSpotlights(members) {
+    const container = document.querySelector('.cards-container');
+    container.innerHTML = ''; // this is for cleaning tle last
+
+    members.forEach(member => {
+        const card = document.createElement('div');
+        card.classList.add('card1');
+        card.innerHTML = `
+            <h3>${member.names}</h3>
+            <p>${member.legalRepresentative}</p>
+            <p>${member.phone[0].phone1}</p>
+            <p>${member.websiteURL}</p>
+        `;
+        container.appendChild(card);
+    });
+}
+
+async function getGoldMembers() {
+    const data = await getData(urlJson);
+    if (!data) return;
+    const goldMembers = data.Companies.filter(member => member.membershipLevel === 'Gold');
+    const selectedMembers = selectRandom(goldMembers, 3); 
+    updateMemberSpotlights(selectedMembers);
+}
+
+getGoldMembers();
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -23,12 +23,29 @@ async function apiFetch() {
 
 apiFetch();
 
+// Calculate wind chill
+// this calculate was took from https://www.freemathhelp.com/wind-chill/
+function calculateWindChill(temperature, windSpeed) {
+    if (temperature <= 10 && windSpeed > 4.8) {
+      const windChill =
+        13.12 +
+        0.6215 * temperature -
+        11.37 * Math.pow(windSpeed, 0.16) +
+        0.3965 * temperature * Math.pow(windSpeed, 0.16);
+      return windChill.toFixed(2);
+    } else {
+      return "N/A";
+    }
+  }
+
+  
+
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp.toFixed(0)}&deg;C`;
     weatherIcon.setAttribute('src', `https://openweathermap.org/img/w/${data.weather[0].icon}.png`);
     weatherIcon.setAttribute('alt', data.weather[0].description);
     weatherDescription.textContent = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1);
-    weatherDetails.innerHTML = `Chance of precipitation: ${data.clouds.all}%<br>Humidity: ${data.main.humidity}%<br>Wind: ${data.wind.speed} km/h`;
+    weatherDetails.innerHTML = `Chance of precipitation: ${data.clouds.all}%<br>Humidity: ${data.main.humidity}%<br>Wind: ${data.wind.speed} km/h <br>Wind Chill: ${calculateWindChill(data.main.temp, data.wind.speed)}`;
     const date = new Date(data.dt * 1000);
     const options = { weekday: 'long', hour: 'numeric', minute: 'numeric', hour12: true };
     weatherTime.textContent = date.toLocaleString('en-US', options);
